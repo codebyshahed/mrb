f2e7606a60020dbc2c9cca42ce569454a46d5f71
codebyshahed <shahedevise@gmail.com>
2026-02-06 00:14:40 +0600

    feat: module 3 start
    * 3-1 Module Overview, Primitive and non-primitive data type

---
+++ 1. Intermediate JavaScript & API/module-3 understand common javascript concepts/data_type.js
@@ -0,0 +1,27 @@
+// js is a dynamic types programming language
+let data = 0;
+data = "tormuj";
+data = false;
+data = { name: "vanggi", price: 20 };
+data = [45, 54];
+data = undefined;
+data = null;
+
+console.log(typeof data);
+
+// js primitive data type
+const price = 50;
+const name = "shahed";
+const isStudent = true;
+let isKnown;
+const merit = null;
+console.log(isKnown);
+
+// js non primitive data type
+const student = {
+  name: "badal",
+  location: "bristi goli",
+  subject: "botany",
+};
+
+const friends = ["shahed", "siam", "bappy", "sabbir"];
