16b5eaf4d8248717692dee26a3df097c89ac649f
ownist <shahedevise@gmail.com>
2026-02-21 23:33:42 +0600

    feat: 8-3 JSX, its rules and add dynamic content to JSX in your first component

--- milestone_2_introduction_to_simple_react/module_8_react_core_concept_part_1/explore_core_react/src/App.jsx
+++ milestone_2_introduction_to_simple_react/module_8_react_core_concept_part_1/explore_core_react/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Person />
+      <Person></Person>
+
+      <Person />
+      <Sports />
     </>
-  )
+  );
 }
 
-export default App
+function Person() {
+  const age = 17;
+  const name = "Shahed";
+  return (
+    <h1>
+      I'm {name}, age: {age}
+    </h1>
+  );
+}
+
+function Sports() {
+  return <h4>I love Programming</h4>;
+}
+
+export default App;
