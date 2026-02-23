dd67ff4f936666ffdd716a95acc775a831a740a5
ownist <shahedevise@gmail.com>
2026-02-24 00:42:41 +0600

    feat: 9-7 (Recap) Load dynamic data using async await

---
+++ milestone_2_introduction_to_simple_react/module_9_react_core_concept_part_2/src/User.jsx
@@ -0,0 +1,9 @@
+export default function User({ user }) {
+  // console.log(user);
+
+  return (
+    <div className="">
+      <h3>Name: {user.name}</h3>
+    </div>
+  );
+}
--- milestone_2_introduction_to_simple_react/module_9_react_core_concept_part_2/src/Users.jsx
+++ milestone_2_introduction_to_simple_react/module_9_react_core_concept_part_2/src/Users.jsx
@@ -1,12 +1,19 @@
 import { use } from "react";
+import User from "./User";
 
 export default function Users({ fetchData }) {
   const usersData = use(fetchData);
-  console.log(usersData);
+  // console.log(usersData);
 
   return (
     <div className="">
       <h1>Users: {usersData.length}</h1>
+
+      <div className="">
+        {usersData.map((user, index) => (
+          <User key={index} user={user} />
+        ))}
+      </div>
     </div>
   );
 }
