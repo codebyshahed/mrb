d818e53838f4f4d2bad281cf0dc4904c552d87ab
ownist <shahedevise@gmail.com>
2026-02-25 08:57:33 +0600

    feat: 10-5 Conditional CSS and Toggle state based on click

--- milestone_2_introduction_to_simple_react/module_10_simple_react_rest_countries/src/components/Country/Country.jsx
+++ milestone_2_introduction_to_simple_react/module_10_simple_react_rest_countries/src/components/Country/Country.jsx
@@ -4,35 +4,40 @@ const Country = ({ country }) => {
   const [isVisited, setIsVisited] = useState(false);
 
   const handleVisited = () => {
+    let visitedCountry = 0;
     setIsVisited(true);
   };
 
   return (
-    <div className="p-4 bg-zinc-800/50 space-y-2">
-      <img
-        className="w-full object-contain"
-        src={country.flags.flags.png}
-        alt={country.flags.flags.alt}
-      />
-      <div className="space-y-1">
-        <h1>{country.name.common}</h1>
-        <p>Population: {country?.population?.population}</p>
-        <p>Area: {country?.area?.area}</p>
-        <p>
-          <small>
-            {country?.area?.area >= 300000 ? "Big Country" : "Small Country"}
-          </small>
-        </p>
-
+    <>
+      <div
+        className={`p-4 ${isVisited ? "bg-zinc-500/30" : "bg-zinc-800/50"} flex flex-col justify-between space-y-2`}
+      >
+        <img
+          className="w-full object-contain"
+          src={country.flags.flags.png}
+          alt={country.flags.flags.alt}
+        />
+        <div className="space-y-1">
+          <h1>{country.name.common}</h1>
+          <p>Population: {country?.population?.population}</p>
+          <p>Area: {country?.area?.area}</p>
+          <p>
+            <small>
+              {country?.area?.area >= 300000 ? "Big Country" : "Small Country"}
+            </small>
+          </p>
+        </div>
         <button
           onClick={handleVisited}
           disabled={isVisited}
-          className="px-5 py-2 bg-white text-zinc-800 rounded-sm hover:bg-zinc-300 disabled:bg-zinc-500 disabled:text-zinc-300 disabled:cursor-not-allowed"
+          className={`px-5 py-2 rounded-sm font-normal ${isVisited ? "visited" : "not_visited"}`}
         >
+          {/* px-5 py-2 bg-white text-zinc-800 rounded-sm hover:bg-zinc-300 disabled:bg-zinc-500 disabled:text-zinc-300 disabled:cursor-not-allowed */}
           {isVisited ? "Visited" : "Not Visited"}
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
