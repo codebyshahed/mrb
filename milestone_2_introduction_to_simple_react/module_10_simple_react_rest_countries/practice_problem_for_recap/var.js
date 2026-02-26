02a094d8848d366aa140d64ca6d8dadbefd9d567
ownist <shahedevise@gmail.com>
2026-02-26 23:42:15 +0600

    feat: add variable declaration task

---
+++ milestone_2_introduction_to_simple_react/module_10_simple_react_rest_countries/practice_problem_for_recap/var.js
@@ -0,0 +1,15 @@
+/**
+ * ১. Variable (ভেরিয়েবল)
+ *
+ * টাস্ক: তিনটি ভেরিয়েবল ডিক্লেয়ার করুন: আপনার নাম (string), বয়স (number), এবং আপনি কি ছাত্র কি না (boolean)। এরপর console.log() দিয়ে একটি বাক্যে এগুলো প্রিন্ট করুন।
+ *
+ * Commit Message: feat: add variable declaration task
+ */
+
+let myName = "ownist";
+let age = 15;
+let isStudent = true;
+
+console.log(myName);
+console.log(age);
+console.log(isStudent);
