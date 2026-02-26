e6315cf1f72b47ab1872c7df82deb75b45f3b51c
ownist <shahedevise@gmail.com>
2026-02-26 23:53:38 +0600

    feat: implement multiplication function

---
+++ milestone_2_introduction_to_simple_react/module_10_simple_react_rest_countries/practice_problem_for_recap/function.js
@@ -0,0 +1,16 @@
+/**
+ * ৬. Function (ফাংশন)
+ *
+ * টাস্ক: এমন একটি ফাংশন লিখুন যা দুটি সংখ্যা ইনপুট (parameter) হিসেবে নিবে এবং তাদের গুণফল রিটার্ন করবে। ফাংশনটি কল করে ফলাফল কনসোলে দেখান।
+ *
+ * Commit Message: feat: implement multiplication function
+ */
+
+function multiply(x, y) {
+  return x * y;
+}
+
+const firstMultiplyValue = 2;
+const secondMultiplyValue = 5;
+const result = multiply(firstMultiplyValue, secondMultiplyValue);
+console.log(result);
