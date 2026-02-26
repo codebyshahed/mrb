d692b6763b92aee9885ba9a7136b5821a81412d8
ownist <shahedevise@gmail.com>
2026-02-26 23:50:30 +0600

    feat: define car object and access properties

---
+++ milestone_2_introduction_to_simple_react/module_10_simple_react_rest_countries/practice_problem_for_recap/object.js
@@ -0,0 +1,15 @@
+/**
+ * ৫. Object (অবজেক্ট)
+ *
+ * টাস্ক: car নামে একটি অবজেক্ট তৈরি করুন যেটির প্রোপার্টি হবে: brand, model, এবং year। এরপর অবজেক্ট থেকে শুধু গাড়ির মডেলটি প্রিন্ট করুন।
+ *
+ * Commit Message: feat: define car object and access properties
+ */
+
+const car = {
+  brand: "lamborghini",
+  model: "veneno",
+  year: 2026,
+};
+
+console.log(car.model);
