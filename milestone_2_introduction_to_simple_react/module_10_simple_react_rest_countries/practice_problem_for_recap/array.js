dc2afa656daf184e79ee8833f998c898ed0ec256
ownist <shahedevise@gmail.com>
2026-02-26 23:48:34 +0600

    feat: create fruit array and iterate through items

---
+++ milestone_2_introduction_to_simple_react/module_10_simple_react_rest_countries/practice_problem_for_recap/array.js
@@ -0,0 +1,13 @@
+/**
+ * ৪. Array (অ্যারে)
+ *
+ * টাস্ক: ৫টি ফলের নাম দিয়ে একটি অ্যারে তৈরি করুন। লুপ ব্যবহার করে অ্যারের প্রতিটি ফলের নাম আলাদা আলাদা ভাবে প্রিন্ট করুন।
+ *
+ * Commit Message: feat: create fruit array and iterate through items
+ */
+
+const fol = ["am", "jam", "lichu", "kola", "kathal"];
+
+for (let index = 0; index < fol.length; index++) {
+  console.log(fol[index]);
+}
