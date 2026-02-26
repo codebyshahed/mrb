3e5f1a6fdc6313d5a645d3202440acab42f33edc
ownist <shahedevise@gmail.com>
2026-02-26 23:44:37 +0600

    feat: implement even-odd number checker using conditions

---
+++ milestone_2_introduction_to_simple_react/module_10_simple_react_rest_countries/practice_problem_for_recap/condition.js
@@ -0,0 +1,15 @@
+/**
+ * ২. Condition (কন্ডিশন)
+ *
+ * টাস্ক: একটি নম্বর ভেরিয়েবলে রাখুন। এরপর if-else ব্যবহার করে চেক করুন নম্বরটি জোড় (even) নাকি বিজোড় (odd) এবং সেটি কনসোলে জানান।
+ *
+ * Commit Message: feat: implement even-odd number checker using conditions
+ */
+
+let num = 27;
+
+if (num % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
