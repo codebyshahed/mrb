edb61d0a05b80b37fcbfadffb84adbac522b6e10
ownist <shahedevise@gmail.com>
2026-02-26 23:46:06 +0600

    feat: add loop to print numbers from 1 to 10

---
+++ milestone_2_introduction_to_simple_react/module_10_simple_react_rest_countries/practice_problem_for_recap/loop.js
@@ -0,0 +1,11 @@
+/**
+ * ৩. Loop (লুপ)
+ *
+ * টাস্ক: একটি for লুপ ব্যবহার করে ১ থেকে ১০ পর্যন্ত সবগুলা সংখ্যা কনসোলে প্রিন্ট করুন।
+ *
+ * Commit Message: feat: add loop to print numbers from 1 to 10
+ */
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
