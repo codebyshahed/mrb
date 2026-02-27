111c6caa38b3d93425e2676c3dffe046b1a3e59e
ownist <shahedevise@gmail.com>
2026-02-27 22:26:26 +0600

    feat: 11-4 (Recap) Load Bottles Data and show in grid layout

--- milestone_2_introduction_to_simple_react/module_11_modules_and_data_storage_integration/awesome_water_bottle/src/components/Bottle.jsx
+++ milestone_2_introduction_to_simple_react/module_11_modules_and_data_storage_integration/awesome_water_bottle/src/components/Bottle.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
-const Bottle = ({ bottle }) => {
-  console.log(bottle);
+const Bottle = ({ bottle, handleAddTocart }) => {
+  const { name, img, price, stock } = bottle;
 
   return (
-    <div>
-      <h1>Bootle</h1>
+    <div className="space-y-4">
+      <img src={img} alt="" />
+      <div className="">
+        <h1>{name}</h1>
+        <p>${price}</p>
+        <p>{stock} remaining</p>
+      </div>
 
-      <img width={50} src={bottle.img} alt="" />
+      <div className="">
+        <button
+          onClick={() => handleAddTocart(bottle)}
+          className="px-5 py-2 bg-white text-black"
+        >
+          By Now
+        </button>
+      </div>
     </div>
   );
 };
--- milestone_2_introduction_to_simple_react/module_11_modules_and_data_storage_integration/awesome_water_bottle/src/components/Bottles.jsx
+++ milestone_2_introduction_to_simple_react/module_11_modules_and_data_storage_integration/awesome_water_bottle/src/components/Bottles.jsx
@@ -1,16 +1,25 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Bottle from "./Bottle";
 
 const Bottles = ({ bottlePrimise }) => {
   const bottleData = use(bottlePrimise);
+  const [cart, setCart] = useState([]);
+
+  const handleAddTocart = (bottle) => {
+    console.log(bottle);
+  };
 
   return (
     <div>
-      <h1>Bottles: {bottleData.length}</h1>
+      <h1 className="text-2xl mb-10">Bottles: {bottleData.length}</h1>
 
-      <div className="">
+      <div className="grid grid-cols-3 gap-8">
         {bottleData.map((bottle, index) => (
-          <Bottle bottle={bottle} key={index} />
+          <Bottle
+            handleAddTocart={handleAddTocart}
+            bottle={bottle}
+            key={index}
+          />
         ))}
       </div>
     </div>
