0043ea33056d1169a5c96b4156d209de8431a8ed
ownist <shahedevise@gmail.com>
2026-03-08 19:08:22 +0600

    feat: 14-3 dynamic menu button toggle and menu toggle
    feat(Navbar): Add responsive mobile menu
    - Add lucide-react for menu icons
    - Implement useState for menu toggle
    - Show Menu or CircleX icon based on state
    - Display navigation items for mobile view
    - Hide desktop menu on smaller screens

--- milestone_3_react_router_and_state_management/module_14_tailwindcss_axios_rechart_awesome_components/src/components/Navbar.jsx
+++ milestone_3_react_router_and_state_management/module_14_tailwindcss_axios_rechart_awesome_components/src/components/Navbar.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import { navData } from "../constants";
+import { CircleX, Menu } from "lucide-react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="bg-slate-100 border border-gray-300 w-full">
       <nav className="max-w-285 mx-auto py-1 px-4">
-        <ul className="flex items-center gap-x-8">
+        <div className="lg:hidden">
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            {isOpen ? <CircleX /> : <Menu />}
+          </button>
+
+          <ul className="">
+            {navData.map((item) => (
+              <li key={item.id}>
+                <a href={item.path}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <ul className="hidden lg:flex items-center gap-x-8">
           {navData.map((item) => (
             <li key={item.id}>
               <a href={item.path}>{item.label}</a>
