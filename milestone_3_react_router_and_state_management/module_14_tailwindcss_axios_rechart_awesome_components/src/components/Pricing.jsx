612f17ced1de4cba997027f438370d5a8e2325b8
ownist <shahedevise@gmail.com>
2026-03-08 20:39:03 +0600

    feat: 14-6 Use Daisy UI pricing card to modify for the component
    feat(pricing): Add subscribe button and improve layout
    - Implement subscribe button on pricing cards
    - Adjust grid layout for responsiveness
    - Enhance feature list styling with flexbox
    - Update check icon color

--- milestone_3_react_router_and_state_management/module_14_tailwindcss_axios_rechart_awesome_components/src/components/Pricing.jsx
+++ milestone_3_react_router_and_state_management/module_14_tailwindcss_axios_rechart_awesome_components/src/components/Pricing.jsx
@@ -7,27 +7,31 @@ const Pricing = ({ pricingPromise }) => {
   return (
     <div className="w-full">
       <div className="max-w-285 mx-auto px-4 py-10">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {pricingData.map((items, index) => (
             <div
               key={index}
-              className="p-4 rounded-md border border-gray-200 space-y-2"
+              className="p-4 rounded-md border border-gray-200 space-y-2 flex flex-col"
             >
               <h1 className="text-2xl">{items.planName}</h1>
               <p>
                 ${items.price} USD / {items.duration}
               </p>
 
-              <div className="p-2 rounded bg-slate-100 space-y-2">
+              <div className="p-2 rounded bg-slate-100 space-y-2 flex-1">
                 {items.features.map((feature, index) => (
                   <div key={index} className="flex items-center gap-x-2">
                     <span>
-                      <CircleCheckBig size={15} />
+                      <CircleCheckBig className="text-green-500" size={16} />
                     </span>
-                    <p>{feature}</p>
+                    <p className="text-base">{feature}</p>
                   </div>
                 ))}
               </div>
+
+              <button className="px-4 py-3 w-full rounded bg-zinc-900 cursor-pointer text-slate-200 mt-2.5 duration-200 hover:bg-zinc-700">
+                Subscribe
+              </button>
             </div>
           ))}
         </div>
