aa4a4ef4ccdd068748ae79eb4abc70f8ec653bad
ownist <shahedevise@gmail.com>
2026-03-10 21:37:37 +0600

    feat: 15-2 Understand The Concept of Routing
    feat(router): implement basic React Router setup
    - Configure `createBrowserRouter` for routing
    - Introduce `Root` layout with `Header` and `Outlet`
    - Define nested routes for Home,

---
+++ milestone_3_react_router_and_state_management/module_15_react_router_concept/src/components/Laptop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Laptop = () => {
+  return (
+    <div>
+      <h2>My All Laptops</h2>
+    </div>
+  );
+};
+
+export default Laptop;
--- milestone_3_react_router_and_state_management/module_15_react_router_concept/src/main.jsx
+++ milestone_3_react_router_and_state_management/module_15_react_router_concept/src/main.jsx
@@ -4,15 +4,53 @@ import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from "./components/Root.jsx";
+import Home from "./components/Home.jsx";
+import Mobiles from "./components/Mobiles.jsx";
+import Laptop from "./components/Laptop.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/mobiles",
+        Component: Mobiles,
+      },
+      {
+        path: "laptops",
+        Component: Laptop,
+      },
+    ],
+  },
+  {
+    path: "about",
     element: (
-      <div>
-        <h1>Hello React Router</h1>
+      <div className="">
+        <h1>About me</h1>
       </div>
     ),
+  },
+  {
+    path: "blogs",
+    element: (
+      <div className="">
+        <h1>Amr Blog</h1>
+      </div>
+    ),
+  },
+  {
+    path: "app",
+    Component: App,
+  },
+  {
+    path: "/app2",
+    element: <App />,
   },
 ]);
 
