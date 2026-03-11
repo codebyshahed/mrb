4d5396935b5bd3822c25b795ed9a54b988094c33
ownist <shahedevise@gmail.com>
2026-03-11 17:26:18 +0600

    feat: 15-7 Explore hooks useNavigate and useNavigator for router spinner
    ei module dekhar por matha geche. ki korchi nijew jani nh
    feat(router): add navigation enhancements
    - Display loading state during navigation
    - Add 'Back' button to PostDetails
    - Allow navigating to previous page

--- milestone_3_react_router_and_state_management/module_15_react_router_concept/src/components/Root.jsx
+++ milestone_3_react_router_and_state_management/module_15_react_router_concept/src/components/Root.jsx
@@ -1,14 +1,18 @@
 import Footer from "./Footer";
 // import Header from "./Header";
-import { NavLink, Outlet } from "react-router";
+import { NavLink, Outlet, useNavigation } from "react-router";
 import Sidebar from "./Sidebar";
 // import "./Root.css";
 import Header from "./recap/Header";
 
 const Root = () => {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
+
   return (
     <div>
       <Header />
+      {isNavigating && <span>Loading...</span>}
       <Outlet />
     </div>
   );
--- milestone_3_react_router_and_state_management/module_15_react_router_concept/src/components/recap/PostDetails.jsx
+++ milestone_3_react_router_and_state_management/module_15_react_router_concept/src/components/recap/PostDetails.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { useLoaderData } from "react-router";
+import { useLoaderData, useNavigate } from "react-router";
 
 const PostDetails = () => {
   const userDetails = useLoaderData();
+  const navigate = useNavigate();
+
+  const handleNevigate = () => {
+    navigate("/");
+  };
 
   return (
     <div className="max-w-285 mx-auto px-4 py-10">
       <h1>{userDetails.name}</h1>
       <p>Phone: {userDetails.phone}</p>
+
+      <button onClick={handleNevigate}>Back</button>
+      <br />
+      <br />
+      <button onClick={() => navigate(-1)}>Go Back</button>
     </div>
   );
 };
