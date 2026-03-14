0c5ac40301208c1a73468deb4ce774c242421023
ownist <shahedevise@gmail.com>
2026-03-15 04:14:28 +0600

    feat: 17-7 Store wishlist books to Local Storage and show toast alert
    feat(book-details): enable marking books as read
    
    - Add utility for local storage of read books
    - Implement `add_to_stored_db` and `get_stored_book`
    - Integrate 'Read' button with local storage logic
    - Prevent duplicate book entries

--- milestone_3_react_router_and_state_management/module_17_journey_through_react_router_with_book_vibe/src/pages/BookDetails/BookDetails.jsx
+++ milestone_3_react_router_and_state_management/module_17_journey_through_react_router_with_book_vibe/src/pages/BookDetails/BookDetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router";
+import { add_to_stored_db } from "../../utils/add_to_db";
 
 const BookDetails = () => {
   const { id } = useParams();
@@ -7,13 +8,19 @@ const BookDetails = () => {
 
   const { bookName, image, review } = singleBook;
 
+  const handle_mark_as_read = (id) => {
+    add_to_stored_db(id);
+  };
+
   return (
     <div>
       <img width={100} src={image} alt="book image" />
       <h1>{bookName}</h1>
       <p>{review}</p>
       <div className="">
-        <button className="btn btn-lg">Read</button>
+        <button onClick={() => handle_mark_as_read(id)} className="btn btn-lg">
+          Read
+        </button>
         <button className="btn btn-lg">Wishlist</button>
       </div>
     </div>
---
+++ milestone_3_react_router_and_state_management/module_17_journey_through_react_router_with_book_vibe/src/utils/add_to_db.js
@@ -0,0 +1,24 @@
+const get_stored_book = () => {
+  const stored_book_str = localStorage.getItem("read_list");
+
+  if (stored_book_str) {
+    const stored_book_data = JSON.parse(stored_book_str);
+    return stored_book_data;
+  } else {
+    return [];
+  }
+};
+
+const add_to_stored_db = (id) => {
+  const stored_book_data = get_stored_book();
+
+  if (stored_book_data.includes(id)) {
+    alert("miya.. eita alrady exist");
+  } else {
+    stored_book_data.push(id);
+    const data = JSON.stringify(stored_book_data);
+    localStorage.setItem("read_list", data);
+  }
+};
+
+export { add_to_stored_db };
