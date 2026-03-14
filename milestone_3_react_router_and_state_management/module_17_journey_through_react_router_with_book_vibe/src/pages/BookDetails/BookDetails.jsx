25cdc3d8118ce434627bf8321411f3682e6d24e9
ownist <shahedevise@gmail.com>
2026-03-14 21:36:23 +0600

    feat: 17-6 params, useParams hook and fetching data dynamically with id
    feat(book-details): implement book details page
    - Use loader to fetch book data
    - Get book ID from URL params
    - Display book name, image, and review
    - Add Read and Wishlist buttons

--- milestone_3_react_router_and_state_management/module_17_journey_through_react_router_with_book_vibe/src/pages/BookDetails/BookDetails.jsx
+++ milestone_3_react_router_and_state_management/module_17_journey_through_react_router_with_book_vibe/src/pages/BookDetails/BookDetails.jsx
@@ -1,7 +1,21 @@
+import { useLoaderData, useParams } from "react-router";
+
 const BookDetails = () => {
+  const { id } = useParams();
+  const allBook = useLoaderData();
+  const singleBook = allBook.find((book) => book.bookId === parseInt(id));
+
+  const { bookName, image, review } = singleBook;
+
   return (
     <div>
-      <h1>BooksDetails</h1>
+      <img width={100} src={image} alt="book image" />
+      <h1>{bookName}</h1>
+      <p>{review}</p>
+      <div className="">
+        <button className="btn btn-lg">Read</button>
+        <button className="btn btn-lg">Wishlist</button>
+      </div>
     </div>
   );
 };
