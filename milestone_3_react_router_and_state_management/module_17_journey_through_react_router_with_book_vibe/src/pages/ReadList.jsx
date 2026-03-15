0a25701e1cdb3dbc5a48043d6c17222e9b786204
ownist <shahedevise@gmail.com>
2026-03-15 06:05:57 +0600

    feat: 17-9 Implement sort functionality and Homework
    feat(readlist): Add sorting functionality
    - Implement sorting by total pages
    - Implement sorting by book ratings
    - Add dropdown UI for sort options
    - Update useEffect dependencies

--- milestone_3_react_router_and_state_management/module_17_journey_through_react_router_with_book_vibe/src/pages/ReadList.jsx
+++ milestone_3_react_router_and_state_management/module_17_journey_through_react_router_with_book_vibe/src/pages/ReadList.jsx
@@ -8,6 +8,7 @@ import Book from "./Book/Book";
 const ReadList = () => {
   const [readList, setReadlist] = useState([]);
   const data = useLoaderData();
+  const [sort, setSort] = useState("");
 
   useEffect(() => {
     const stored_book_data = get_stored_book();
@@ -16,7 +17,24 @@ const ReadList = () => {
       converted_stored_books.includes(book.bookId),
     );
     setReadlist(read_list);
-  }, []);
+  }, [data]);
+
+  const handle_sort = (type) => {
+    setSort(type);
+
+    if (type === "pages") {
+      const sorted_by_pages = [...readList].sort(
+        (a, b) => a.totalPages - b.totalPages,
+      );
+      setSort(sorted_by_pages);
+    }
+    if (type === "ratings") {
+      const sorted_by_ratings = [...readList].sort(
+        (a, b) => a.rating - b.rating,
+      );
+      setSort(sorted_by_ratings);
+    }
+  };
 
   return (
     <div>
@@ -25,6 +43,18 @@ const ReadList = () => {
           <Tab>Read List</Tab>
           <Tab>My Wishlist</Tab>
         </TabList>
+
+        <details className="dropdown">
+          <summary className="btn m-1">Sort By {sort ? sort : ""}</summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+              <a onClick={() => handle_sort("pages")}>Pages</a>
+            </li>
+            <li>
+              <a onClick={() => handle_sort("ratings")}>Ratings</a>
+            </li>
+          </ul>
+        </details>
 
         <TabPanel>
           <h2>Book lists {readList.length}</h2>
